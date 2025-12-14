b9d8ec716a910f29831e5dba20d252221e40c5c7
dkalchenko <dkalchenko2@gmail.com>
2025-12-14 14:20:08 +0200

    fix: redirect if authorized

--- DedicatedGeo.Mono.Api/Controllers/Web/WebPublicController.cs
+++ DedicatedGeo.Mono.Api/Controllers/Web/WebPublicController.cs
@@ -2,11 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DedicatedGeo.Mono.Api.Controllers.Web;
 
-public class WebPublicController: Controller
+public class WebPublicController : Controller
 {
     [HttpGet("/")]
     public IActionResult Login()
     {
+        // If the user is authenticated/authorized, redirect them to the admin map.
+        if (User.Identity?.IsAuthenticated == true)
+        {
+            return Redirect("/admin/map");
+        }
+
         return View();
-    }   
+    }
 }
